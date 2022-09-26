d0eec4eec279ab6ebec4f692ddd681f49e724ce9
Huss <57395585+anefiox@users.noreply.github.com>
2022-09-26 12:46:15 +0100

    Adding GET endpoint

--- ContractsApi/V1/Controllers/ContractsApiController.cs
+++ ContractsApi/V1/Controllers/ContractsApiController.cs
@@ -1,9 +1,12 @@
 using ContractsApi.V1.Boundary.Response;
+using ContractsApi.V1.Infrastructure;
 using ContractsApi.V1.UseCase.Interfaces;
 using Hackney.Core.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ContractsApi.V1.Controllers
 {
@@ -15,12 +18,38 @@ namespace ContractsApi.V1.Controllers
     //TODO: rename class to match the API name
     public class ContractsApiController : BaseController
     {
-        private readonly IGetAllUseCase _getAllUseCase;
-        private readonly IGetByIdUseCase _getByIdUseCase;
-        public ContractsApiController(IGetAllUseCase getAllUseCase, IGetByIdUseCase getByIdUseCase)
+        private readonly IGetContractByIdUseCase _getByIdUseCase;
+        //private readonly IPostNewContractUseCase _postNewContractUseCase;
+        public ContractsApiController(IGetContractByIdUseCase getByIdUseCase)
         {
-            _getAllUseCase = getAllUseCase;
             _getByIdUseCase = getByIdUseCase;
+        }
+
+        /// <summary>
+        /// Retrieves the asset with the supplied id
+        /// </summary>
+        /// <response code="200">Successfully retrieved details for the specified ID</response>
+        /// <response code="404">No tenure information found for the specified ID</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(ContractResponseObject), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpGet]
+        [Route("{id}")]
+        [LogCall(LogLevel.Information)]
+        public async Task<IActionResult> GetContractById([FromRoute] GetContractByIdRequest query)
+        {
+            var result = await _getAssetByIdUseCase.ExecuteAsync(query).ConfigureAwait(false);
+            if (result == null) return NotFound(query.Id);
+
+            var eTag = string.Empty;
+            if (result.VersionNumber.HasValue)
+                eTag = result.VersionNumber.ToString();
+
+            HttpContext.Response.Headers.Add(HeaderConstants.ETag, EntityTagHeaderValue.Parse($"\"{eTag}\"").Tag);
+
+            return Ok(result);
         }
 
         //TODO: add xml comments containing information that will be included in the auto generated swagger docs (https://github.com/LBHackney-IT/lbh-base-api/wiki/Controllers-and-Response-Objects)
