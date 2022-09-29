52cf954e62c17a63040828432830a9e37353288f
Miles-Alford <miles.alford@hackney.gov.uk>
2022-09-29 15:19:44 +0100

    Create contract database class and conversion factories

---
+++ ContractsApi/V1/Infrastructure/ContractsDb.cs
@@ -0,0 +1,40 @@
+using Amazon.DynamoDBv2.DataModel;
+using Hackney.Core.DynamoDb.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using ContractsApi.V1.Domain;
+
+namespace ContractsApi.V1.Infrastructure
+{
+    [DynamoDBTable("Contracts", LowerCamelCaseProperties = true)]
+    public class ContractDb
+    {
+        [DynamoDBHashKey]
+        public Guid Id { get; set; }
+
+        [DynamoDBProperty]
+        public string TargetType { get; set; }
+
+        [DynamoDBProperty]
+        public string ContractType { get; set; }
+
+        [DynamoDBProperty(Converter = typeof(DynamoDbDateTimeConverter))]
+        public DateTime? StartDate { get; set; }
+
+        [DynamoDBProperty(Converter = typeof(DynamoDbDateTimeConverter))]
+        public DateTime? EndDate { get; set; }
+
+        [DynamoDBProperty(Converter = typeof(DynamoDbDateTimeConverter))]
+        public DateTime? RenewalDate { get; set; }
+
+        [DynamoDBProperty(Converter = typeof(DynamoDbObjectListConverter<RelatedPeople>))]
+        public List<RelatedPeople> RelatedPeople { get; set; } = new List<RelatedPeople>();
+
+        [DynamoDBProperty(Converter = typeof(DynamoDbObjectListConverter<Charges>))]
+        public List<Charges> Charges { get; set; } = new List<Charges>();
+
+        [DynamoDBVersion]
+        public int? VersionNumber { get; set; }
+    }
+}
