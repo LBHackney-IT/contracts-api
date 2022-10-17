9152d5e5bad810bf6e59f09e3024d330b2deaae1
Miles-Alford <miles.alford@hackney.gov.uk>
2022-10-17 13:01:34 +0100

    Update base controller tests to use xunit

--- ContractsApi.Tests/V1/Controllers/BaseControllerTests.cs
+++ ContractsApi.Tests/V1/Controllers/BaseControllerTests.cs
@@ -1,25 +1,22 @@
 using System.Collections.Generic;
 using ContractsApi.V1.Controllers;
-using ContractsApi.V1.Infrastructure;
 using FluentAssertions;
-using Hackney.Core.Middleware;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Routing;
-using NUnit.Framework;
+using Xunit;
+using HeaderConstants = Hackney.Core.Middleware.HeaderConstants;
 
 namespace ContractsApi.Tests.V1.Controllers
 {
-    [TestFixture]
     public class BaseControllerTests
     {
         private BaseController _sut;
         private ControllerContext _controllerContext;
         private HttpContext _stubHttpContext;
 
-        [SetUp]
-        public void Init()
+        public BaseControllerTests()
         {
             _stubHttpContext = new DefaultHttpContext();
             _controllerContext = new ControllerContext(new ActionContext(_stubHttpContext, new RouteData(), new ControllerActionDescriptor()));
@@ -28,7 +25,7 @@ namespace ContractsApi.Tests.V1.Controllers
             _sut.ControllerContext = _controllerContext;
         }
 
-        [Test]
+        [Fact]
         public void GetCorrelationShouldThrowExceptionIfCorrelationHeaderUnavailable()
         {
             // Arrange + Act + Assert
@@ -37,11 +34,11 @@ namespace ContractsApi.Tests.V1.Controllers
                 .WithMessage("Request is missing a correlationId");
         }
 
-        [Test]
+        [Fact]
         public void GetCorrelationShouldReturnCorrelationIdWhenExists()
         {
             // Arrange
-            _stubHttpContext.Request.Headers.Add(Hackney.Core.Middleware.HeaderConstants.CorrelationId, "123");
+            _stubHttpContext.Request.Headers.Add(HeaderConstants.CorrelationId, "123");
 
             // Act
             var result = _sut.GetCorrelationId();
