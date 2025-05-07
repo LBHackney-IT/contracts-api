fac242247e463d615f51c616a2c58403b8356f3d
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-05-07 15:41:16 +0100

    add initial tf config for pre-prod

---
+++ terraform/pre-production/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
+  default_tags = {
+    Name              = "contracts-api-${var.environment_name}"
+    Environment       = var.environment_name
+    terraform-managed = true
+    project_name      = var.project_name
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "housing-pre-production-terraform-state"
+    encrypt        = true
+    region         = "eu-west-2"
+    key            = "services/contracts-api/state"
+    dynamodb_table = "housing-pre-production-terraform-state-lock"
+  }
+}
