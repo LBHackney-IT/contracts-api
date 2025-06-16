100122f16104d0f9a7a903af5e2049b3c4b49358
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2025-06-16 17:13:29 +0100

    ContractManagement - rename property, not sure what I was thinking

--- ContractsApi/V1/Domain/ContractManagement.cs
+++ ContractsApi/V1/Domain/ContractManagement.cs
@@ -5,6 +5,6 @@ namespace ContractsApi.V1.Domain
     public class ContractManagement
     {
         public AssetHierarchy AssetHierarchy { get; set; }
-        public Guid? ParentAssetId { get; set; }
+        public Guid? ParentContractId { get; set; }
     }
 }
