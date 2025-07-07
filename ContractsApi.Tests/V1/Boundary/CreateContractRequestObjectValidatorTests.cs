e1e9dd4a8b25f2399234c632d96f258817b1fb4b
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2025-07-07 15:02:06 +0100

    Better boundary validation - business logic

--- ContractsApi.Tests/V1/Boundary/CreateContractRequestObjectValidatorTests.cs
+++ ContractsApi.Tests/V1/Boundary/CreateContractRequestObjectValidatorTests.cs
@@ -22,14 +22,6 @@ namespace ContractsApi.Tests.V1.Boundary.Request.Validation
         }
 
         [Fact]
-        public void RequestShouldErrorForNoContractHierarchy()
-        {
-            var model = new CreateContractRequestObject() { ContractManagement = null };
-            var result = _ccrov.TestValidate(model);
-            result.ShouldHaveValidationErrorFor(x => x.ContractManagement)
-            .WithErrorMessage("ContractManagement must be present upon contract creation");
-        }
-        [Fact]
         public void RequestShouldErrorForInvalidEnumValue()
         {
             var model = new CreateContractRequestObject()
@@ -42,6 +34,36 @@ namespace ContractsApi.Tests.V1.Boundary.Request.Validation
             var result = _ccrov.TestValidate(model);
             result.ShouldHaveValidationErrorFor(x => x.ContractManagement.ContractHierarchy)
             .WithErrorMessage("The hierarchy provided is not valid");
+        }
+        [Fact]
+        public void RequestShouldErrorIfHierarchyIsBlockOrStandaloneAndParentContractIdIsProvided()
+        {
+            var model = new CreateContractRequestObject()
+            {
+                ContractManagement = new()
+                {
+                    ContractHierarchy = (ContractHierarchy) 1,
+                    ParentContractId = new Guid()
+                }
+            };
+            var result = _ccrov.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.ContractManagement.ParentContractId)
+            .WithErrorMessage("ParentContractId must be empty for Blocks and Standalone Units");
+        }
+        [Fact]
+        public void RequestShouldErrorIfHierarchyIsChildUnitAndParentContractIdIsNotProvided()
+        {
+            var model = new CreateContractRequestObject()
+            {
+                ContractManagement = new()
+                {
+                    ContractHierarchy = (ContractHierarchy) 2,
+                    ParentContractId = null
+                }
+            };
+            var result = _ccrov.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.ContractManagement.ParentContractId)
+            .WithErrorMessage("ParentContractId must be provided for Child Units");
         }
     }
 }
