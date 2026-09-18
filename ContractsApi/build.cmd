9e49226d477edb9548e2c3061d35502ba5a5148a
LBHTKarki <tuomo.karki@hackney.gov.uk>
2026-09-18 15:48:50 +0100

    upgrade to dotnet 10

--- ContractsApi/build.cmd
+++ ContractsApi/build.cmd
@@ -1,2 +1,2 @@
 dotnet restore
-dotnet lambda package --configuration release --framework net8.0 --output-package bin/release/net8.0/contracts-api.zip
+dotnet lambda package --configuration release --framework net10.0 --output-package bin/release/net10.0/contracts-api.zip
